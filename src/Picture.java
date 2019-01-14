f124cd0f6c380c945b198ec8e9a3e5619aa93cc3
Yashrif Hasib <33043549+yashrifhasib@users.noreply.github.com>
2019-01-14 14:18:49 -0500

    Update Picture.java
    A9

--- src/Picture.java
+++ src/Picture.java
@@ -385,28 +385,54 @@ public class Picture extends SimplePicture
         this.write("collage.jpg");        
     }
     
+    public void edgeDetection2(int edgeDist) {
+        Pixel currentPixel = null;
+        Pixel rightPixel = null;
+        Pixel bottomPixel = null;
+        Pixel[][] pixels = this.getPixels2D();
+        Color rightColor = null;
+        Color bottomColor = null;
+        
+        for (int col = 0; col < pixels[0].length; col++) {
+            for (int row = 0; row < pixels.length - 1; row++) {
+                currentPixel = pixels[row][col];
+                rightPixel = pixels[row][col + 1];
+                bottomPixel = pixels[row + 1][col];
+                rightColor = rightPixel.getColor();
+                bottomColor = bottomPixel.getColor();
+                
+                if ((currentPixel.colorDistance(rightColor) > edgeDist || (currentPixel.colorDistance(bottomColor) > edgeDist))
+                    currentPixel.setColor(Color.BLACK);
+                else
+                    currentPixel.setColor(Color.WHITE);
+            }
+        }
+    }
+    
     /** Method to show large changes in color
      * @param edgeDist the distance for finding edges
      */
     public void edgeDetection(int edgeDist)
     {
-        Pixel leftPixel = null;
+        Pixel currentPixel = null;
         Pixel rightPixel = null;
+        Pixel bottomPixel = null;
         Pixel[][] pixels = this.getPixels2D();
         Color rightColor = null;
-        for (int row = 0; row < pixels.length; row++)
-        {
-            for (int col = 0;
-                 col < pixels[0].length-1; col++)
-            {
-                leftPixel = pixels[row][col];
-                rightPixel = pixels[row][col+1];
+        Color bottomColor = null;
+        
+        for (int row = 0; row < pixels.length - 1; row++) {
+            for (int col = 0; col < pixels[0].length - 1; col++) {
+                currentPixel = pixels[row][col];
+                rightPixel = pixels[row][col + 1];
+                bottomPixel = pixels[row + 1][col];
                 rightColor = rightPixel.getColor();
-                if (leftPixel.colorDistance(rightColor) >
-                        edgeDist)
-                    leftPixel.setColor(Color.BLACK);
+                bottomColor = bottomPixel.getColor();
+                
+                if ((currentPixel.colorDistance(rightColor) > edgeDist || (currentPixel.colorDistance(bottomColor) > edgeDist))
+                    currentPixel.setColor(Color.BLACK);
                 else
-                    leftPixel.setColor(Color.WHITE);
+                    currentPixel.setColor(Color.WHITE);
             }
         }
     }
